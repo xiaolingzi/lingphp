936c0c54d86e024459ca819060cbf136dca8738a
zhouyongling <yonglingzhou@imscv.com>
2017-06-20 17:06:27 +0800

    update

--- application/APP/Global.php
+++ application/APP/Global.php
@@ -1,6 +1,6 @@
 <?php
 //项目根目录
-define("ROOT_PATH", dirname(realpath($_SERVER['SCRIPT_NAME'])));
+define("ROOT_PATH", dirname(realpath($_SERVER['SCRIPT_FILENAME'])));
 //框架目录
 define("FRAME_PATH", dirname(dirname(__FILE__)));
 //环境变量
